d647aad61c11be53c327ee1a27bbddc26407061e
Eduardo Gadelha <eduardo.gadelha@ort.org.br>
2025-09-22 00:02:54 -0300

    inicio tela do perfil Desenvolver mais amanhã commitar

---
+++ Telas/telaPerfil.js
@@ -0,0 +1,19 @@
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+  ScrollView,
+  Image,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+
+export default function TelaPerfil(){
+    return(
+        <ScrollView>
+            <Text>Tela do perfil</Text>
+        </ScrollView>
+    );
+}
